b4c0ac11168d36202adcddca3e1f4dbe4339bcfc
Darwin Solano <vivas@media-interactive.de>
2022-11-10 18:39:06 -0500

    Carga de proyecto Foront React

---
+++ src/components/Login.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react'
+import { Menu } from './Menu'
+
+export const Login = () => {
+
+  const [miLogin, setMiLogin] = useState("false");
+  const [miUser, setMiUser] = useState("");
+  const [miPass, setMiPass] = useState("");
+
+  function iniciarSesion(e){
+    e.preventDefault();
+    let usuario = document.getElementById("txtusu").value;
+    let contrasena = document.getElementById("txtpass").value;
+    if (usuario.length === 0 || contrasena.length === 0){
+        alert("Complete los datos de manera correcta");
+    }
+    else{
+        if( miUser === "admin" && miPass === "123"){
+            setMiLogin("true");
+            document.getElementById("form_login").style.display = "none";
+        }
+        else{
+            setMiLogin("false");
+            alert("Error de usuario y/o contraseña!!!")
+            document.getElementById("txtusu").value = "";
+            document.getElementById("txtpass").value = "";
+            document.getElementById("txtusu").focus();
+        }
+    }
+
+  }
+
+
+  return (
+    <div className="container" style={{background: "ligthgray", marginTop:20, padding:20 }}>
+        <form id="form_login">
+            <div>
+                <h1>Login</h1>
+                <label>Username:  </label>
+                <input type="text" id="txtusu" style={{textAling:"center"}} onBlur={(e) => setMiUser(e.target.value)} className="form-control" required />
+            </div>
+            <div>
+                <label>Password:  </label>
+                <input type="password" id="txtpass" style={{textAling:"center"}} onBlur={(e) => setMiPass(e.target.value)} className="form-control" required />
+            </div>
+            <br/>
+            <input type="submit" className="btn btn-primary" value="Ingresar" onClick={iniciarSesion}></input>
+        </form>
+
+        { miLogin === "true" && <Menu usuario={miUser} /> }
+
+    </div>
+  )
+}
---
+++ src/components/Menu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Menu = (props) => {
+  return (
+    <div>
+        <h1>Menu Inicial</h1>
+        <h2>Bienvenido : {props.usuario}</h2>
+    </div>
+    
+  )
+}
